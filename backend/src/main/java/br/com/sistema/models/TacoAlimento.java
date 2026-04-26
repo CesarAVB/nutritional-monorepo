d2276a478a9bbb2a2909ec0ac757844cbebe6db5
César Augusto <cesar.augusto.rj1@gmail.com>
2026-04-26 15:34:10 -0300

    fix: corrige nome da tabela e ajusta anotações das colunas em TacoAlimento

--- backend/src/main/java/br/com/sistema/models/TacoAlimento.java
+++ backend/src/main/java/br/com/sistema/models/TacoAlimento.java
@@ -16,13 +16,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "tbl_taco_alimentos")
+@Table(name = "tbl_alimentos")
 public class TacoAlimento {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    //@Column(nullable = false, unique = true, name = "codigo_taco")
     private Integer codigoTaco;
 
     @Column(nullable = false, length = 100)
@@ -34,28 +35,28 @@ public class TacoAlimento {
     @Column(precision = 6, scale = 2)
     private BigDecimal umidade;
 
-    @Column(precision = 8, scale = 2)
+    @Column(precision = 8, scale = 2, name = "energia_kcal")
     private BigDecimal energiaKcal;
 
-    @Column(precision = 8, scale = 2)
+    @Column(precision = 8, scale = 2, name = "energia_kj")
     private BigDecimal energiaKj;
 
-    @Column(precision = 6, scale = 2)
+    @Column(precision = 6, scale = 2, name = "proteina_g")
     private BigDecimal proteinaG;
 
-    @Column(precision = 6, scale = 2)
+    @Column(precision = 6, scale = 2, name = "lipideos_g")
     private BigDecimal lipideosG;
 
-    @Column(precision = 6, scale = 2)
+    @Column(precision = 6, scale = 2, name = "colesterol_mg")
     private BigDecimal colesterolMg;
 
-    @Column(precision = 6, scale = 2)
+    @Column(precision = 6, scale = 2, name = "carboidratos_g")
     private BigDecimal carboidratosG;
 
-    @Column(precision = 6, scale = 2)
+    @Column(precision = 6, scale = 2, name = "fibra_alimentar_g")
     private BigDecimal fibraAlimentarG;
 
-    @Column(precision = 6, scale = 2)
+    @Column(precision = 6, scale = 2, name = "cinzas_g")
     private BigDecimal cinzasG;
 
     @Column(columnDefinition = "TEXT")
