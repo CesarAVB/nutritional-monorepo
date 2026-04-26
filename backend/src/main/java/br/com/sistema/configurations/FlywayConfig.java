301c01a704f14c1166ca113ba1389cc3431df2d6
César Augusto <cesar.augusto.rj1@gmail.com>
2026-04-26 20:05:22 -0300

    feat: adiciona configuração do Flyway para migrações de banco de dados

---
+++ backend/src/main/java/br/com/sistema/configurations/FlywayConfig.java
@@ -0,0 +1,17 @@
+package br.com.sistema.configurations;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+
+    @Bean
+    FlywayMigrationStrategy repairThenMigrate() {
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
+        };
+    }
+}
